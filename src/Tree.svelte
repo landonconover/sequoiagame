206d7323f6d323d1d802781fee969b7e14607760
Landon Conover <landon.conover@gmail.com>
2021-04-05 10:37:17 -0600

    Init Commit with start

---
+++ src/Tree.svelte
@@ -0,0 +1,18 @@
+<div>
+    <p>Tree {treeNum}</p>
+    <p>First: {firstToken}</p>
+    <p>Second: {secondToken}</p>
+</div>
+
+<style>
+    div {
+        border: 3px solid black;
+        margin: 10px;
+    }
+</style>
+
+<script>
+    export let treeNum;
+    export let firstToken;
+    export let secondToken;
+</script>
